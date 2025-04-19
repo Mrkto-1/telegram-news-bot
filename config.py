695333edf42ea6b8255a0e727612eb29063c99d1
Mrkto-1 <frolovkolyafr@gmail.com>
2025-04-20 01:00:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,21 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = "@worldnews_ua2025"
 
 ACTIVE_HOURS = (6, 24)
+
 BLACKLIST = ["спорт", "мода", "музика", "кіно", "розваги", "серіали", "футбол"]
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # не використовується поки
+DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")    # теж ні
 
+# ДОДАНІ RSS з українських економічних джерел
 RSS_FEEDS = [
-    "https://feeds.reuters.com/reuters/businessNews",
-    "https://www.cnbc.com/id/100003114/device/rss/rss.html",
-    "https://www.marketwatch.com/rss/topstories",
+    "https://feeds.reuters.com/reuters/businessNews",                # англомовне
+    "https://www.cnbc.com/id/100003114/device/rss/rss.html",         # англомовне
+    "https://www.marketwatch.com/rss/topstories",                    # англомовне
+    
+    "https://www.epravda.com.ua/rss/",                                # 🇺🇦 Економічна правда
+    "https://www.ukrinform.ua/rss/economics",                         # 🇺🇦 Укрінформ
+    "https://biz.liga.net/ekonomika/rss.xml",                         # 🇺🇦 Liga.net
+    "https://mind.ua/rss/news",                                       # 🇺🇦 Mind.ua
+    "https://forbes.ua/rss"                                           # 🇺🇦 Forbes Україна
 ]
